8e416cfef9d052572110700aad8a65acacf35580
Mattias Axelsson <mattiasaxelsson@icloud.com>
2024-09-18 14:47:26 +0200

    added some styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './../styles/Navbar.css';
+import doglogo from './../assets/logo.png';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
+      <div className="navbar-brand">
+        <img src={doglogo} alt="Doggydaycare" />
+      </div>
+      <ul className="navbar-links">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/CatalogOfDogs">Dog Catalog</Link>
+          <Link to="/catalogOfDogs">Dog Catalog</Link>
         </li>
       </ul>
     </nav>
