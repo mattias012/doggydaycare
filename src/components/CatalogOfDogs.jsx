e1f24eda5e65b79e36b01c58c602c99973699571
Mattias Axelsson <mattiasaxelsson@icloud.com>
2024-09-18 14:04:40 +0200

    added basic structure

---
+++ src/components/CatalogOfDogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CatalogOfDogs() {
+  return (
+    <div className="catalog-of-dogs">
+      <h1>Dog Catalog</h1>
+      <p>Here you will find all the dogs registered with our daycare.</p>
+    </div>
+  );
+}
+
+export default CatalogOfDogs;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Doggy Daycare!</h1>
+      <p>This is your dog's second home.</p>
+    </div>
+  );
+}
+
+export default Home;
