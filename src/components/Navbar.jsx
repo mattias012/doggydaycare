593af6b2b03f41d83ab61fafbad84acd5078a8a6
Mattias Axelsson <mattiasaxelsson@icloud.com>
2024-09-20 00:49:50 +0200

    fixed navbar filterbar home dog catalogofdogs css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,37 @@
-
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './../styles/Navbar.css';
 import doglogo from './../assets/logo.png';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false); // State for controlling the collapsible menu
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Toggle the menu state between open and closed
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false); // Close the menu after clicking a link
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <img src={doglogo} alt="Doggydaycare" />
+        <img src={doglogo} alt="Doggy Daycare" />
       </div>
-      <ul className="navbar-links">
+
+      {/* Hamburger icon that appears on smaller screens */}
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776; {/* Hamburger icon */}
+      </div>
+
+      {/* Links in navbar */}
+      <ul className={`navbar-links ${isOpen ? '' : 'collapsed'}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>Home</Link>
         </li>
         <li>
-          <Link to="/catalogOfDogs">Dog Catalog</Link>
+          <Link to="/catalogOfDogs" onClick={closeMenu}>Dog Catalog</Link>
         </li>
       </ul>
     </nav>
