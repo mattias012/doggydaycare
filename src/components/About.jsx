e8df6e030f05ea49ffa8cd02435c831b671dcefd
Mattias Axelsson <mattiasaxelsson@icloud.com>
2024-09-25 11:48:39 +0200

    added home styles and about page

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import './../styles/Home.css'; //We use the same CSS for this page
+
+function About() {
+  return (
+    <div className="home">
+      <h1>Welcome to Doggy Daycare!</h1>
+      <p>This is your dog's second home.</p>
+    </div>
+  );
+}
+
+export default Home;
